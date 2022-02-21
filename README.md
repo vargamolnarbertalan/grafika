3a0abb3d92839ed9eb20e23237f387f4a3275e4d
vargamolnarbertalan <vargamolnarb@gmail.com>
2022-02-21 21:52:24 +0100

    mapparendszer rendezése

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # grafika
+
+Varga-Molnár Bertalan | PY7QFH - Számítógép grafika előrehaladás
---
+++ gyakorlat/gyak1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Ezen a gyakorlaton feltelepitettuk az alabbiakat:\n");
+    printf("-git\n");
+    printf("-vscode\n");
+    printf("-gcc\n");
+    return 0;
+}
