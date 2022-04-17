1d887d5e9cae2cc525be4367b8dcb508828e39b7
vargamolnarbertalan <vargamolnarb@gmail.com>
2022-04-17 19:16:37 +0200

    fenyek, anyagjellemzok

--- gyakorlat/gyak7/cube/src/scene.c
+++ gyakorlat/gyak7/cube/src/scene.c
@@ -27,7 +27,7 @@ void init_scene(Scene* scene)
 
 void set_lighting()
 {
-    float ambient_light[] = { 1.0f, 1.0f, 0.0f, 1.0f }; //sarga megvilagitas
+    float ambient_light[] = { 1.0f, 1.0f, 0.0f, 1.0f }; //sarga  megvilagitas
     float diffuse_light[] = { 1.0f, 1.0f, 1.0, 1.0f };
     float specular_light[] = { 0.0f, 0.0f, 0.0f, 1.0f };
     float position[] = { 0.0f, 10.0f, 10.0f, 1.0f }; //megvilagitas y10 z10
